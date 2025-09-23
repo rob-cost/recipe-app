38b51ee6ccfbf0438cdee3e890a8db355c2598b4
rob-cost <robertocostantino3@gmail.com>
2025-09-23 11:13:52 +0200

    updated model logic

--- src/recipe/models.py
+++ src/recipe/models.py
@@ -7,21 +7,11 @@ class Recipe (models.Model):
     ingredients = models.TextField(help_text="Insert ingredients separated by a comma")
     cooking_time = models.PositiveIntegerField(validators=[MinValueValidator(1)], help_text="Insert the cooking time in minutes")
     difficulty = models.CharField(max_length=30, editable=False)
-    method = models.TextField()
-    likes = models.IntegerField(default=0)
+    method = models.TextField(help_text="Describe the cooking method of the recipe")
+    likes = models.PositiveIntegerField(default=0)
 
-    def clean(self):
-
-        #make sure that name field is not empty
-        if not self.name:
-            raise ValidationError("Name cannot be empty")
-        
-        #make sure that ingredients are well formatted and not empty
+    def calculateDifficulty(self):
         items = [i.strip() for i in self.ingredients.split(",") if i.strip()]
-        if not items:
-            raise ValidationError("Ingredients must be separated by a comma")
-        
-        #calculate difficulty
         if self.cooking_time < 10 and len(items) < 4:
             self.difficulty = "Easy"
         elif self.cooking_time < 10 and len(items) >= 4:
@@ -30,12 +20,21 @@ class Recipe (models.Model):
             self.difficulty = "Intermediate"
         elif self.cooking_time >= 10 and len(items) >= 4:
             self.difficulty = "Hard"
+
+    def clean(self):
+        #make sure that name field is not empty
+        if not self.name:
+            raise ValidationError("Name cannot be empty")
+        
+        #make sure that ingredients are well formatted and not empty
+        items = [i.strip() for i in self.ingredients.split(",") if i.strip()]
+        if not items:
+            raise ValidationError("Ingredients must be separated by a comma")
        
     def save(self, *args, **kwargs):
         self.full_clean() 
+        self.calculateDifficulty()
         super().save(*args, **kwargs)
         
-
-
     def __str__(self):
         return str(self.name)
