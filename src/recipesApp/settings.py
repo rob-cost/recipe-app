0a3c447314d87a1d0cff6d62a82fddff65a4bb2c
rob-cost <robertocostantino3@gmail.com>
2025-10-30 15:15:58 +0100

    Setup Cloudinary for production

--- src/recipesApp/settings.py
+++ src/recipesApp/settings.py
@@ -35,6 +35,15 @@ ALLOWED_HOSTS = ['*']
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+}
+
+
 
 # Application definition
 
@@ -46,6 +55,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'recipe',
+    'cloudinary',
+    'cloudinary_storage',
 ]
 
 MIDDLEWARE = [
