5ad4581bebbd615b27002f757f0eb817f8211aff
rob-cost <robertocostantino3@gmail.com>
2025-12-30 08:33:04 +0100

    Updated search_view function

--- src/recipe/views.py
+++ src/recipe/views.py
@@ -115,14 +115,14 @@ def toggle_like(request, recipe_id):
 @login_required
 def search_view(request):
     form = RecipeSearchForm(request.GET or None)
-    chart_bar = None
-    chart_pie = None
-    chart_line = None
 
     context = {
     'form': form,
     'recipes': None,
     'recipes_count': 0,
+    'chart_bar': None,
+    'chart_pie': None,
+    'chart_line': None,
 }
     
     if request.GET and form.is_valid():
@@ -181,13 +181,14 @@ def search_view(request):
 
                     chart_line = get_chart('line', line_df)
 
-                    context.push = {
+                    context.update = {
                         'chart_bar': chart_bar,
                         'chart_pie': chart_pie,
                         'chart_line': chart_line,
                     }
             except Exception as e:
                 messages.error(request, f'Error generating charts: {str(e)}')
+                # Updated search_view function
     
     return render(request, 'recipe/recipes_search.html', context)
 
